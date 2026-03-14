cc95576ed95f2e069bf39b6b0c9afb80cd799b80
Sophie-melon <sophie.hy.geng@icloud.com>
2026-03-14 14:49:21 -0500

    Add comment about robot speed variable
    Added comment explaining the purpose of the variable

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -3,6 +3,7 @@ lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
+This is to simulate a change made on a robot: robot_speed = 5 # m/s
 """
 
 def main():
@@ -11,7 +12,10 @@ def main():
     name = "Sophie Geng" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-    print("Hello everyone! I'm Sophie, a sophomore from Austin, Texas. In my free time I love reading and listening to true crime podcasts. I also play harp and tennis! Currently at school I really enjoy chemistry and math, but I'm still not sure what I want to study in college.")
+    print("Hello everyone! I'm Sophie, a sophomore from Austin, Texas. 
+           In my free time I love reading and listening to true crime podcasts. 
+           I also play harp and tennis! Currently at school I really enjoy chemistry and math, 
+           but I'm still not sure what I want to study in college.")
 
 if __name__ == "__main__":
     main()
